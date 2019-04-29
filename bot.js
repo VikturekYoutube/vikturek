2239e1ea98d5a714ba4b7bf4e2b8ce42f51b6178
VikturekYoutube <46503199+VikturekYoutube@users.noreply.github.com>
2019-04-29 21:40:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,31 +2,13 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 ;
-const TOKEN = "NTMyMjk1MjMyNjY3MTIzNzQy.Dy0NHg.sNbXaWIiKymWNU2UF4CTu3p0FaY";
+const TOKEN = "NTcyNDk3NzcwMjI5OTIzODUw.XMdOzg.CIuAtklHvpmataWCez_I0LGKkwQ";
 const prefix = "v!"
 client.on('ready', function() {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', function(message) {
-    console.log(message.content);
-});
 
-client.on('message', function(message){
-    if (message.author.equals(client.user)) return;
-  
-    if (message.content == ("v!help") 
-        message.channel.sendMessage("``v!social - socialni site vikturka!``";
-    }
-});
-
-client.on('message', function(message) {
-    if (message.author.equals(client.user)) return;
-
-    if (message.content == ("v!social")
-        message.channel.sendMessage("Socialni sitě vikturka! Youtube: https://www.youtube.com/channel/UCKXU6VIZdB0CHbNkHChdKxA Instagram: https://www.instagram.com/vikturek.love.you/ Twitter: https://www.twitter.com/VikturekG")
-    }
-});
 
 client.login(TOKEN);
 
